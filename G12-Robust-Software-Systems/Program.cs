50e8aee52ce6ee6ee2948859c13df756232de1eb
Skuli <skulifh@gmail.com>
2014-04-10 16:17:26 +0200

    added the simulation thingy

--- G12-Robust-Software-Systems/Program.cs
+++ G12-Robust-Software-Systems/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,24 @@ namespace G12_Robust_Software_Systems
 {
     class Program
     {
+
+        public double Divide(double number, double divisor)
+        {
+            Contract.Requires<ArgumentOutOfRangeException>(
+                divisor != 0,
+                "Divide by zero is not allowed."
+                );
+
+            return number / divisor;
+        }
+
         static void Main(string[] args)
         {
+            Program dafuq = new Program();
+            double blabla = dafuq.Divide(5, 2);
+            Console.WriteLine(blabla);
+            System.Threading.Thread.Sleep(10000);
+
         }
     }
 }
