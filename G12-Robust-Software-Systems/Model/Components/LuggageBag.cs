2042a3a142c66c0e1ddf8a21872654c90928262f
Rune Thor Maartensson <rune@strontium90.dk>
2014-05-01 11:54:27 +0200

    Add luggage.

---
+++ G12-Robust-Software-Systems/Model/Components/LuggageBag.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G12_Robust_Software_Systems.Model.Components
+{
+    class LuggageBag
+    {
+        private IComponent destination;
+        public LuggageBag(IComponent destination)
+        {
+            this.destination = destination;
+        }
+    }
+}
