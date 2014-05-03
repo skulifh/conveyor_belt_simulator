b0c4f9b70c9556191ad6711962a8661021e0e1d5
Paw Petersen <kapaw89@gmail.com>
2014-05-03 14:34:47 +0200

    more contractz

--- G12-Robust-Software-Systems/Model/LuggageProcessing/Sink.cs
+++ G12-Robust-Software-Systems/Model/LuggageProcessing/Sink.cs
@@ -1,6 +1,7 @@
 ﻿using G12_Robust_Software_Systems.Model.Components;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,11 +14,15 @@ namespace G12_Robust_Software_Systems.Model.LuggageProcessing
         private int counter;
         public Sink(ILuggageQueue queue)
         {
+            Contract.Requires(queue != null, "queue cannot be null");
+
             this.queue = queue;
             this.counter = 0;
         }
         public void processLuggage(LuggageBag luggage)
         {
+            Contract.Requires(luggage != null, "luggage cannot be null");
+
             List<LuggageBag> receivedLuggage = this.queue.checkLuggageQueue();
             counter += receivedLuggage.Count();
         }
