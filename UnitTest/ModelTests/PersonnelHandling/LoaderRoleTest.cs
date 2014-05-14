5dc2eca5174ac9c70b7e84d37b7dc7486b628704
Paw Petersen <kapaw89@gmail.com>
2014-05-14 20:16:09 +0200

    testing

---
+++ UnitTest/ModelTests/PersonnelHandling/LoaderRoleTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using G12_Robust_Software_Systems.Model.PersonnelHandling;
+
+namespace UnitTest.ModelTests.PersonnelHandling
+{
+    [TestClass]
+    public class LoaderRoleTest
+    {
+        [TestMethod]
+        public void LoaderRoleTest1()
+        {
+            LoaderRole role = new LoaderRole();
+            bool pass1 = role.Equals(new LoaderRole());
+
+            Assert.IsTrue(pass1);
+        }
+    }
+}
---
+++ UnitTest/ModelTests/PersonnelHandling/StuckLuggageRoleTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using G12_Robust_Software_Systems.Model.PersonnelHandling;
+
+namespace UnitTest.ModelTests.PersonnelHandling
+{
+    [TestClass]
+    public class StuckLuggageRoleTest
+    {
+        [TestMethod]
+        public void StuckLuggageRoleTest1()
+        {
+            StuckLuggageRole role = new StuckLuggageRole();
+            bool pass1 = role.Equals(new StuckLuggageRole());
+
+            Assert.IsTrue(pass1);
+        }
+    }
+}
