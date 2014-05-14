d8c7adbef96d4636db837b500240d6867faa2b2b
Paw Petersen <kapaw89@gmail.com>
2014-05-14 18:51:46 +0200

    a

---
+++ UnitTest/ModelTests/PersonnelHandling/PersonnelControllerTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using G12_Robust_Software_Systems.Model.PersonnelHandling;
+
+namespace UnitTest.ModelTests.PersonnelHandling
+{
+    [TestClass]
+    public class PersonnelControllerTest
+    {
+        [TestMethod]
+        public void PersonnelControllerTest1()
+        {
+            List<Personnel> personnel = new List<Personnel>();
+            personnel.add();
+        }
+    }
+}
---
+++ UnitTest/ModelTests/PersonnelHandling/PersonnelTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using G12_Robust_Software_Systems.Model.PersonnelHandling;
+using System.Collections.Generic;
+
+namespace UnitTest.ModelTests.PersonnelHandling
+{
+    [TestClass]
+    public class PersonnelTest
+    {
+        [TestMethod]
+        public void PersonnelTest1()
+        {
+            List<IRole> roles = new List<IRole>();
+            roles.Add(new XRayRole());
+            Personnel p1 = new Personnel(1,roles);
+            bool pass1 = roles == p1.get_roles();
+            
+            IRole loaderrole = new LoaderRole();
+            roles.Add(loaderrole);
+            p1.add_role(loaderrole);
+            bool pass2 = roles == p1.get_roles();
+
+            roles.Remove(loaderrole);
+            p1.remove_role(new LoaderRole());
+            p1.add_role(loaderrole);
+            bool pass3 = roles == p1.get_roles();
+
+            Assert.IsTrue(pass1);
+            Assert.IsTrue(pass2);
+            Assert.IsTrue(pass3);
+        }
+    }
+}
